4bc2edf0995e16d383832636a4f3690b47c81597
poohtankina <167339023+poohtankina@users.noreply.github.com>
2024-09-10 10:01:32 +0800

    2nd change

--- src/routes/mydata.js
+++ src/routes/mydata.js
@@ -5,8 +5,8 @@ const router = Router();
 router.get("/mydata", (req, res) => {
   const subject = "ITE 314";
   const exam = "P2";
-  const nodejs = "NODEJS";
-  res.status(200).send({ subject, exam, nodejs });
+  const node = "NODEJS";
+  res.status(200).send({ subject, exam, node });
 });
 
 export default router;
